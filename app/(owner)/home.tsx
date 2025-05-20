978b0064495896ca59f590e6307afaea05cc6ec7
unknown <kantosarobidyratolojanahary@gmail.com>
2025-05-20 22:42:39 +0300

    folder structure

---
+++ app/(owner)/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const OwnerHome = () => {
+  return (
+    <View>
+      <Text>OwnerHome</Text>
+    </View>
+  )
+}
+
+export default OwnerHome
